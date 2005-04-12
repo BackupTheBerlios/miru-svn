a2c21d7ceda557ca0c3f9e4cc7aa4af08cb54f0d
darren <darren@a9569a3e-21f2-0310-b6f4-f35a5ec6ad9e>
2005-04-12 15:07:30 +0000

    Added Sax wrappers.

---
+++ trunk/src/main/java/org/iterx/sax/InputSource.java
@@ -0,0 +1,70 @@
+/*
+  org.iterx.sax.InputSource
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  Copyright (C)2004-2005 Darren Graves <darren@iterx.org>
+  All Rights Reserved.  
+*/
+package org.iterx.sax;
+
+import java.io.Reader;
+import java.io.InputStream;
+
+public class InputSource extends org.xml.sax.InputSource {
+
+    private Object object;
+
+    public InputSource() {}
+
+    public InputSource(Object object) {
+
+        this.object = object;
+    }
+
+    public InputSource(InputStream byteStream) {
+
+        super(byteStream);
+    }
+
+    public InputSource(InputStream byteStream, String encoding) {
+
+        super(byteStream);
+        setEncoding(encoding);
+    }
+
+    public InputSource(Reader characterStream) {
+        
+        super(characterStream);
+    }
+
+    public InputSource(String systemId) {
+
+        super(systemId);
+    }
+
+
+    public Object getObject() {
+
+        return object;
+    }
+
+    public void setObject(Object object) {
+        
+        this.object = object;
+    }
+
+}
+
